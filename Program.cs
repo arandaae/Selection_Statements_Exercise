ae4208e6b7524383aeca590037374078384aec18
Armando Aranda <arandaae19@gmail.com>
2022-04-26 00:50:24 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Selection_Statements_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess my favorite number.");
+            Console.WriteLine();
+
+            int favNumber = 7;
+        
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Guess is too low.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Guess is to high.");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
+            Console.Clear();
+
+            SchoolSubject();
+            static void SchoolSubject()
+            {
+                Console.WriteLine("What is your favorite school subject");
+                Console.WriteLine("Math, History, English, Biology, Chemistry");
+                string subject = Console.ReadLine();
+
+                switch (subject)
+                {
+                    case "Math":
+                        Console.WriteLine("Math is your favorite subject!");
+                        break;
+                    case "History":
+                        Console.WriteLine("History is your favorite subject!");
+                        break;
+                    case "English":
+                        Console.WriteLine("English is your favorite subject!");
+                        break;
+                    case "Biology":
+                        Console.WriteLine("Biology is your favorite subject!");
+                        break;
+                    case "Chemistry":
+                        Console.WriteLine("Chemistry is your favorite subject!");
+                        break;
+                    default:
+                        Console.WriteLine("That subject is not included here.");
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
